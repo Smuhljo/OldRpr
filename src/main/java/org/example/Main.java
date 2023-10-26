74f2a81b008feba24077c1ff21f36082f8649f1b
Muhamed Selmanovic <Muhamed@diri.no>
2023-10-26 21:57:42 +0200

    zadatak

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,82 @@
-package org.example;
+class Sat {
+    private int sati, minute, sekunde;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public Sat(int sati, int minute, int sekunde) {
+        Postavi(sati, minute, sekunde);
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public void Postavi(int sati, int minute, int sekunde) {
+        this.sati = sati;
+        this.minute = minute;
+        this.sekunde = sekunde;
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public void Sljedeci() {
+        sekunde++;
+        if (sekunde == 60) {
+            sekunde = 0;
+            minute++;
         }
+        if (minute == 60) {
+            minute = 0;
+            sati++;
+        }
+        if (sati == 24) {
+            sati = 0;
+        }
+    }
+
+    public void Prethodni() {
+        sekunde--;
+        if (sekunde == -1) {
+            sekunde = 59;
+            minute--;
+        }
+        if (minute == -1) {
+            minute = 59;
+            sati--;
+        }
+        if (sati == -1) {
+            sati = 23;
+        }
+    }
+
+    public void PomjeriZa(int pomak) {
+        if (pomak > 0) {
+            for (int i = 0; i < pomak; i++) {
+                Sljedeci();
+            }
+        } else {
+            for (int i = 0; i < -pomak; i++) {
+                Prethodni();
+            }
+        }
+    }
+
+    public int DajSate() {
+        return sati;
+    }
+
+    public int DajMinute() {
+        return minute;
+    }
+
+    public int DajSekunde() {
+        return sekunde;
+    }
+
+    public void Ispisi() {
+        System.out.println(sati + ":" + minute + ":" + sekunde);
+    }
+
+    public static void main(String[] args) {
+        Sat s = new Sat(15, 30, 45);
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.PomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0, 0, 0);
+        s.Ispisi();
     }
 }
